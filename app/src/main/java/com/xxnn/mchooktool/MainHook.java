fc54945847497fc4fdac3697ffa82513209b3a5c
duweiguan <duweiguan@sinohealth.cn>
2022-02-14 17:16:20 +0800

    feat: 创建hookReceivePacket方法

--- app/src/main/java/com/xxnn/mchooktool/MainHook.java
+++ app/src/main/java/com/xxnn/mchooktool/MainHook.java
@@ -9,6 +9,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
  * @date 2022/2/14 16:06
  */
 public class MainHook implements IXposedHookLoadPackage {
+    private static boolean isInit = false;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -20,22 +21,46 @@ public class MainHook implements IXposedHookLoadPackage {
         try {
             // 开始hook方法
             // 参数1: class路径, 参数3: 方法名
+
+            // hook初始化函数, 强制打开调试模式
             XposedHelpers.findAndHookMethod("com.tencent.qphone.base.util.CodecWarpper",
-                    lpparam.classLoader, "getInfo", new XC_MethodHook() {
+                    lpparam.classLoader, "init", new XC_MethodHook() {
                         // 执行方法之前执行的方法
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            super.beforeHookedMethod(param);
+                            if (param.args.length >= 2) {
+                                param.args[1] = true;
+                                if (!isInit) {
+                                    hookReceivePacket(param.thisObject.getClass());
+                                    isInit = true;
+                                }
+                            }
                         }
+                    });
 
+            XposedHelpers.findAndHookMethod("com.tencent.qphone.base.util.CodecWarpper",
+                    lpparam.classLoader, "onReceData", new XC_MethodHook() {
                         // 执行方法之后执行的方法
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            super.afterHookedMethod(param);
+                            if (!isInit) {
+                                hookReceivePacket(param.thisObject.getClass());
+                                isInit = true;
+                            }
                         }
                     });
         } catch (Throwable e) {
             XposedBridge.log(e);
         }
+    }
+
+    private void hookReceivePacket(Class<?> clazz) {
+        XC_MethodHook xcMethodHook = new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                return;
+            }
+        };
+        XposedBridge.hookAllMethods(clazz, "onResponse", xcMethodHook);
     }
 }
